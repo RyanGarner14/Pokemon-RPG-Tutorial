9120b5b011c28ed1e7cf0d6ff9b7896d86036b10
RyanGarner14 <RyanGarner14@Gmail.com>
2021-03-06 19:16:57 +0000

    Grid Based Movement, Collisions, Basic Encounter Logic, Scriptable Objects
    -Added 2D Grid Based movement
    -Added Solid Objects layer to collide with player
    -Added Grass Layer for player to pass through causing "encounter" 1/10 times
    -Added Scriptable Objects of Pokemon

---
+++ Pokemon RPG Tutorial/Assets/Scripts/Pokemon/Pokemon.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pokemon
+{
+    PokemonBase _base;
+    int level;
+
+    public Pokemon(PokemonBase pBase, int pLevel)
+    {
+        _base = pBase;
+        level = pLevel;
+    }
+
+    public int MaxHP
+    {
+        get { return getStatFromLevel(_base.MaxHP) + 5; }
+    }
+    public int Attack
+    {
+        get { return getStatFromLevel(_base.Attack); }
+    }
+    public int Defense
+    {
+        get { return getStatFromLevel(_base.Defense); }
+    }
+    public int SpAttack
+    {
+        get { return getStatFromLevel(_base.SpAttack); }
+    }
+    public int SpDefense
+    {
+        get { return getStatFromLevel(_base.SpDefense); }
+    }
+    public int Speed
+    {
+        get { return getStatFromLevel(_base.Speed); }
+    }
+
+    private int getStatFromLevel(int stat)
+    {
+        return (Mathf.FloorToInt((stat * level) / 100f) + 5);
+    }
+}
